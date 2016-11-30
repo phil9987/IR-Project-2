f1921c9096904157e4cbb0e50db173646afcaf25
Marc Fischer <spam@totallyawesome.at>
2016-11-30 14:16:28 +0100

    started to implmement metrics

--- src/main/scala/QueryMetric.scala
+++ src/main/scala/QueryMetric.scala
@@ -1,6 +1,37 @@
+import scala.collection.mutable
+import scala.collection.mutable.MutableList
+import scala.io.Source
 /**
   * Created by marc on 30/11/16.
   */
-class QueryMetric {
-  //TODO: implement me
+object QueryMetric {
+
+  private val logger = new Logger("QueryMetric")
+  val codeToQuery = new mutable.HashMap[Int, String]
+  val codeToJudgement = new mutable.HashMap[Int, MutableList[(String, Int)]]
+
+  def init() = {
+    logger.log("reading questions")
+    var i : Int = -1
+    for (line <- Source.fromFile("./src/main/resources/questions-descriptions.txt").getLines()) {
+      if (line.startsWith("<num>"))
+        i = Integer.parseInt(line.replaceAll("[^0-9]*", ""))
+      if (line.startsWith("<title>"))
+      {
+        val title = line.replace("<title> Topic: ", "").trim
+        codeToQuery(i) = title
+      }
+    }
+
+    logger.log("reading relevance judgements")
+    for (line <- Source.fromFile("./src/main/resources/relevance-judgements.csv").getLines()) {
+      val tokens = line.split(' ')
+      val id = Integer.parseInt(tokens(0))
+      codeToJudgement(id)  = codeToJudgement.getOrElse(id, new MutableList[(String, Int)]) :+ ((tokens(2), Integer
+        .parseInt
+        (tokens(3)) ))
+    }
+  }
+  init()
+
 }
--- src/main/scala/SearchEngine.scala
+++ src/main/scala/SearchEngine.scala
@@ -5,8 +5,17 @@ object SearchEngine{
     val dr = new DocumentReader(wp)
     val ii = new DefaultInvertedIndex(dr)
     val rm = new DefaultRankingModel(ii, wp)
+    val logger = new Logger("SearchEngine")
 
     rm.query("the cat jumps asdf elephant".split(' ').toList)
+    println(QueryMetric.codeToQuery(50))
+
+    for(queryId <- QueryMetric.codeToQuery.keys)
+      {
+        val query = QueryMetric.codeToQuery(queryId)
+        val ranking = rm.query(query.split(' ').toList)
+        //TODO evalute
+      }
 
   }
 }
